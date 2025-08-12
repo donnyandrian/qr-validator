6e383ef90c901b8f7398c8769bea28f7f8a3e5cc
xellanix <107340353+xellanix@users.noreply.github.com>
2025-08-12 19:33:54 +0700

    feat(gatherDriveFile): Downscale downloaded image

--- helpers/gatherDriveFile.ts
+++ helpers/gatherDriveFile.ts
@@ -3,19 +3,57 @@
 import { createCanvas, loadImage } from "@napi-rs/canvas";
 import fs from "fs";
 
+type Size = {
+    width: number;
+    height: number;
+};
+
+const scaleByHeight = {
+    "800": (w: number, h: number): Size => ({
+        width: (w * 800) / h,
+        height: 800,
+    }),
+    "840": (w: number, h: number): Size => ({
+        width: (w * 840) / h,
+        height: 840,
+    }),
+    "900": (w: number, h: number): Size => ({
+        width: (w * 900) / h,
+        height: 900,
+    }),
+};
+
+const scaleByRatio = (w: number, h: number): Size => {
+    const ratio = w / h;
+    switch (ratio) {
+        case 0.67:
+            return scaleByHeight["900"](w, h);
+        case 0.83:
+        case 0.71:
+            return scaleByHeight["840"](w, h);
+        default:
+            return scaleByHeight["800"](w, h);
+    }
+};
+
 export async function generateImage(path: string, name: string) {
     try {
         const templateImage = await loadImage(path);
 
-        const canvas = createCanvas(templateImage.width, templateImage.height);
+        const rescaled = scaleByRatio(
+            templateImage.width,
+            templateImage.height,
+        );
+
+        const canvas = createCanvas(rescaled.width, rescaled.height);
         const ctx = canvas.getContext("2d");
-        ctx.drawImage(templateImage, 0, 0);
+        ctx.drawImage(templateImage, 0, 0, canvas.width, canvas.height);
 
         const blob = await canvas.convertToBlob({ mime: "image/png" });
 
         // Blob to file
         fs.writeFileSync(
-            "./public/ignored/" + name,
+            "./public/ignored/orkess4/" + name,
             Buffer.from(await blob.arrayBuffer()),
         );
     } catch (error) {
