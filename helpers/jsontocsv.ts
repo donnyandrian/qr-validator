c00a807554f7bfbd932c28eadf6f2098f97cd17e
xellanix <107340353+xellanix@users.noreply.github.com>
2025-08-12 11:17:48 +0700

    fix(jsonTocsv): Prevent processing empty array

--- helpers/jsontocsv.ts
+++ helpers/jsontocsv.ts
@@ -1,8 +1,21 @@
 import fs from "fs";
 import { format } from "@fast-csv/format";
 
-function jsonToCsv2(data: any[], output: string): void {
-    const headers = Object.keys(data[0]);
+type Item = {
+    id: string;
+    data: string;
+    status: string;
+    validatorName: string;
+    validatedAt: string;
+};
+
+function jsonToCsv(data: Item[], output: string): void {
+    if (data.length < 1) {
+        console.log("No data found.");
+        return;
+    }
+
+    const headers = Object.keys(data[0]!);
 
     const stream = format({ headers });
     stream.pipe(fs.createWriteStream(output));
@@ -11,18 +24,20 @@ function jsonToCsv2(data: any[], output: string): void {
         stream.write(
             headers.map((header) =>
                 header !== "validatedAt"
-                    ? row[header]
+                    ? row[header as keyof Item]
                     : new Date(row[header]).toLocaleString(),
             ),
         );
     }
 
     stream.end();
+
+    console.log(`Data saved to ${output}`);
 }
 
 function main() {
-    const data = JSON.parse(fs.readFileSync("history.json", "utf-8"));
-    jsonToCsv2(data, "history.csv");
+    const data = JSON.parse(fs.readFileSync("history.json", "utf-8")) as Item[];
+    jsonToCsv(data, "history.csv");
 }
 
 main();
