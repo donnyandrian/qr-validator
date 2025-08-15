3a8a8771e01335da4d3a7b39366b2c0a9751b285
xellanix <107340353+xellanix@users.noreply.github.com>
2025-08-15 20:45:46 +0700

    fix(data): Update server configurations

--- src/data/orkess4/server.ts
+++ src/data/orkess4/server.ts
@@ -2,7 +2,9 @@ import "server-only";
 import type { DataType } from "~/data/orkess4/type";
 import type { ValidationType } from "~/schemas/validations/orkess4";
 
-export const data = {} as Record<string, DataType>;
-
+// Used in validation
 export const inputKey: keyof ValidationType = "nim";
-export const datasetKey = "NIM";
+// Used in report generation or related
+export const datasetKey: keyof DataType = "NIM";
+// Path to the dataset
+export const datasetPath = "./src/data/orkess4/db.csv";
