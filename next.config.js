1aef043e5c5137aec0359fb0ad1e8b9c457cebaa
xellanix <107340353+xellanix@users.noreply.github.com>
2025-08-17 21:16:12 +0700

    chore: Add simple allowed dev origin for cloudflared

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
     serverExternalPackages: ["@napi-rs/canvas"],
+    allowedDevOrigins: ["*"],
 };
 
 export default config;
